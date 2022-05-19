dc646ca102c5b2c8e775cebf5fc2dd6c22ad4685
Johann Bandelow <johannabandelow@gmail.com>
2022-05-19 12:42:58 -0300

    Primeiro commit, inicio do projeto

---
+++ src/main/java/com/johannbandelow/mtgcardapi/MtgCardApiApplication.java
@@ -0,0 +1,13 @@
+package com.johannbandelow.mtgcardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgCardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgCardApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johannbandelow/mtgcardapi/MtgCardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johannbandelow.mtgcardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtgCardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
